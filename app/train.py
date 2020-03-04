d0d05af59661ec57f1fbc01fdce297fe2bbacb92
Michail Roesli <misha.roesli@gmail.com>
2020-03-04 12:27:52 -0800

    move neuralnet to algs and generalize train usage

--- app/train.py
+++ app/train.py
@@ -1,53 +1,55 @@
-numIters = 100000
-numEps = 1000
-competeEps = 100
-threshold = 0.55
-height = 11
-width = 11
-player_cnt = 8
+import json
+import os
+import sys
 
-from game import Game
-from agent import Agent
+DEFAULT_TRAIN_CONFIG_PATH = "../settings/train_params"
+DEFAULT_ALGS_PATH = "../algs/"
 
-# https://web.stanford.edu/~surag/posts/alphazero.html
+
 def train():
-    # initialise neural network
-    nnet = NNet()
-    # for training, all agents uses the same nnet
-    # unless we want to use a evolution algorithm
-    agents = [Agent(nnet, training = True) for _ in range(player_cnt)]
-    for i in range(numIters):
-        for e in range(numEps):
-            # collect examples from a new game
-            g = Game(height, width, player_cnt)
-            winner_id = g.run(agents)
-            for agent in agents:
-                # return a new trained nnet
-                X = agent.records
-                # need to get a good loss function
-                Y = 0
-                new_nnet = nnet.trainNNet(X, Y)
-        # compare new net with previous net
-        frac_win = compete(new_nnet, nnet)
-        if frac_win > threshold:
-            # replace with new net
-            nnet = new_nnet
-    return nnet
+    pass
 
-def compete(nnet1, nnet2):
-    agents = [None] * player_cnt
-    sep = player_cnt//2
-    for i in range(sep):
-        agents[i] = Agent(nnet1)
-    for i in range(sep, player_cnt):
-        agents[i] = Agent(nnet2)
-    wins = 0
-    for _ in range(competeEps):
-        g = Game(height, width, player_cnt)
-        if g.run(agents) < sep:
-            wins += 1
-    return win/competeEps
 
-if __name__ == '__main__':
-    nnet = train()
-    # need to store the nnet
+def usage():
+    print("Usage: python train.py [path/to/config/file] trial_name")
+
+
+def main():
+    # check if there is an input file
+    if len(sys.argv) > 3 or len(sys.argv) < 2:
+        print("Incorrect usage...")
+        usage()
+        exit(0)
+
+    if len(sys.argv) == 3:
+        # parse the input json file and run the associated training alg
+        try:
+            with open(sys.argv[1]+".json", "r") as config_file:
+                config = json.load(config_file)
+        except FileNotFoundError:
+            print("No configuration file found. Using default.")
+            with open(DEFAULT_TRAIN_CONFIG_PATH+".json", "r") as config_file:
+                config = json.load(config_file)
+        trial = sys.argv[2]
+    else:
+        trial = sys.argv[1]
+
+    try:
+        algorithm = config[trial]['algorithm']
+        iterations = config[trial]['iterations']
+    except Exception:
+        print("Missing training parameter")
+        exit(0)
+
+    # if os.path.exists(DEFAULT_ALGS_PATH+"/"+algorithm):
+    #     os.system("python "+model_path+" "+data_dir_path+" "+results_dir_path)
+
+    # use them as classes?
+
+    # check if alg = nn or other and run the associated algorithm
+
+    # compare its performance with the previous
+
+
+if __name__ == "__main__":
+    main()
