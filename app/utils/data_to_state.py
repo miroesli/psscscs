cb5a3758884af62f19833e9ec4970b799f3f6f49
BuildTools <misha.roesli@gmail.com>
2020-03-04 23:39:49 -0800

    linting

--- app/utils/data_to_state.py
+++ app/utils/data_to_state.py
@@ -8,23 +8,25 @@ HUNGER_a = -100
 HUNGER_m = 0.005
 SNAKE_m = 0.01
 
+
 def translate(data):
     """ Preprocess the data
-    
+
     Args:
         data: the data defined by the
                 Battlesnake Snake API (2020.01)
                 https://docs.battlesnake.com/snake-api
-    
+
     Return:
         state: the game state defined by game.py
-    
+
     """
 
     height = data['board']['height']
     width = data['board']['width']
 
-    state = [[[EMPTY] * width for row in range(height)] for layer in range(max_snakes)]
+    state = [
+        [[EMPTY] * width for row in range(height)] for layer in range(max_snakes)]
 
     health = data['you']['health']
     dist = len(data['you']['body'])
@@ -33,7 +35,7 @@ def translate(data):
         dist -= 1
     for food in data['board']['food']:
         state[0][food['y']][food['x']] = EMPTY + (health + HUNGER_a) * HUNGER_m
-    
+
     i = 1
     for snake in data['board']['snakes']:
         health = snake['health']
@@ -46,7 +48,9 @@ def translate(data):
                 print("\n\n\n\n\n\n")
             dist -= 1
         for food in data['board']['food']:
-            state[i][food['y']][food['x']] = EMPTY + (health + HUNGER_a) * HUNGER_m
+            state[i][food['y']][food['x']] = EMPTY + \
+                (health + HUNGER_a) * HUNGER_m
         i += 1
-    
-    return state #, (data['you']['body'][0]['y'], data['you']['body'][0]['x'])
+
+    # , (data['you']['body'][0]['y'], data['you']['body'][0]['x'])
+    return state
