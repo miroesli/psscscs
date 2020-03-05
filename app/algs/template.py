db76f94706b2b50c98c8119b124585f7ec6f74a3
BuildTools <misha.roesli@gmail.com>
2020-03-05 00:04:11 -0800

    update template initialization

--- app/algs/template.py
+++ app/algs/template.py
@@ -1,6 +1,8 @@
 class template:
-    def __init__(self, **config):
-        self.iterations = config['iterations']
+    def __init__(self, iterations=100, **config):
+        # default iterations are 100, but if specified in config, it is
+        # overwritten.
+        self.iterations = iterations
         print("Initialized configuration...")
 
     def train(self):
