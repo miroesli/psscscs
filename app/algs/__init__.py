50f8d3d0e707c87a28ec8321971a7924273c389f
BuildTools <misha.roesli@gmail.com>
2020-03-04 23:39:09 -0800

    remove automatic import

--- app/algs/__init__.py
+++ app/algs/__init__.py
@@ -1,5 +1,9 @@
-from os.path import dirname, basename, isfile, join
-import glob
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [basename(f)[:-3] for f in modules if isfile(f)
-           and not f.endswith('__init__.py')]
+from algs.neuralnet import *
+from algs.template import *
+
+# TODO: Get algs to import classes automatically
+# import glob
+# from os.path import dirname, basename, isfile, join
+# modules = glob.glob(join(dirname(__file__), "*.py"))
+# __all__ = [basename(f)[:-3] for f in modules if isfile(f)
+#            and not f.endswith('__init__.py')]
